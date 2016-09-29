505eb9dcbb6e431c0b8d03541d464443c758f892
gunnlaugur <gju@kth.se>
2016-09-29 11:22:10 +0200

    added connector code

--- src/dist1/db/DBConnector.java
+++ src/dist1/db/DBConnector.java
@@ -1,10 +1,16 @@
 
 package dist1.db;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Gunnlaugur, Mats
  */
 public class DBConnector {
+    private Connection con;
     
+    public DBConnector() {
+
+    }
 }
