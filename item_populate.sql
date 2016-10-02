d56add0624f0b403a01b8f3c921e5c5d702c4fba
gunnlaugur <gju@kth.se>
2016-10-02 13:58:26 +0200

    sql updates

---
+++ item_populate.sql
@@ -0,0 +1,8 @@
+use store;
+
+insert into Item (item_name, item_type, price, quantity) values ('milk', 'drink', 10, 25);
+insert into Item (item_name, item_type, price, quantity) values ('cola', 'drink', 15, 100);
+insert into Item (item_name, item_type, price, quantity) values ('water', 'drink', 5, 10);
+insert into Item (item_name, item_type, price, quantity) values ('chicken', 'food', 30, 5);
+insert into Item (item_name, item_type, price, quantity) values ('cookies', 'food', 15, 30);
+insert into Item (item_name, item_type, price, quantity) values ('hammer', 'tool', 30, 6);
