b1e8f9d3bcdfadada3386cb3a5478b1641ccf698
mats <mats@bajs.bajs>
2016-10-04 12:44:42 +0200

    todos

--- src/dist1/bo/BasketFacade.java
+++ src/dist1/bo/BasketFacade.java
@@ -23,8 +23,7 @@ public class BasketFacade {
     public static ItemView[] getItemsRequest(TokenView tv) {
         BasketLogic bl = new BasketLogic();
         Item[] items = bl.getItems(tv);
-        
-        //TODO check what happens if items is empty
+
         ItemView[] iv = new ItemView[items.length];
         for(int i = 0; i < items.length; i++) {
             iv[i] = new ItemView(items[i].getName(),items[i].getType(),items[i].getPrice(),items[i].getQuantity(),items[i].getId());
